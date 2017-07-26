cd7a2f79c74e437c8ee8cb2e4ac334f73e361ead
Merlin Beutlberger <m.beutlberger@sap.com>
2017-07-26 17:01:21 +0200

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,101 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+
+		connect: {
+			options: {
+				port: 40000,
+				hostname: '*'
+			},
+			src: {},
+			dist: {}
+		},
+
+		openui5_connect: {
+			options: {
+				resources: [
+					'bower_components/openui5-sap.ui.core/resources',
+					'bower_components/openui5-sap.m/resources',
+					'bower_components/openui5-themelib_sap_belize/resources'
+				],
+				testresources: [
+					'bower_components/openui5-sap.ui.core/test-resources',
+					'bower_components/openui5-sap.m/test-resources',
+					'bower_components/openui5-themelib_sap_belize/test-resources'
+				]
+			},
+			src: {
+				options: {
+					appresources: 'webapp'
+				}
+			},
+			dist: {
+				options: {
+					appresources: 'dist'
+				}
+			}
+		},
+
+		openui5_preload: {
+			component: {
+				options: {
+					resources: {
+						cwd: 'webapp',
+						prefix: 'sap/ui/demo/todo',
+						src: [
+							'**/*.js',
+							'**/*.fragment.html',
+							'**/*.fragment.json',
+							'**/*.fragment.xml',
+							'**/*.view.html',
+							'**/*.view.json',
+							'**/*.view.xml',
+							'**/*.properties',
+							'manifest.json',
+							'!test/**'
+						]
+					},
+					dest: 'dist'
+				},
+				components: true
+			}
+		},
+
+		clean: {
+			dist: 'dist'
+		},
+
+		copy: {
+			dist: {
+				files: [ {
+					expand: true,
+					cwd: 'webapp',
+					src: [
+						'**',
+						'!test/**'
+					],
+					dest: 'dist'
+				} ]
+			}
+		}
+	});
+
+	// These plugins provide necessary tasks.
+	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-openui5');
+
+	// Server task
+	grunt.registerTask('serve', function(target) {
+		grunt.task.run('openui5_connect:' + (target || 'src') + ':keepalive');
+	});
+
+	// Build task
+	grunt.registerTask('build', ['clean:dist', 'openui5_preload', 'copy']);
+
+	// Default task
+	grunt.registerTask('default', ['serve']);
+};
---
+++ README.md
@@ -0,0 +1,16 @@
+![OpenUI5 logo](./OpenUI5_Logo.png)
+
+# teched17-openui5-sample-app
+> Based on [OpenUI5 sample app](https://github.com/SAP/openui5-sample-app)
+
+This repository was used in the SAP TechEd 2017 session **DX261** in which one learns how to add continuous integration features to a SAPUI5 application.
+
+It represents the [OpenUI5 sample app](https://github.com/SAP/openui5-sample-app) at different states of this process:
+
+Branch | State
+------ | -------
+**master** | [OpenUI5 sample app](https://github.com/SAP/openui5-sample-app) without any tests or continuous integration features
+**solution-add-and-configure-eslint** | Added static code check based on [ESLint](https://eslint.org/)
+**solution-add-unit-tests-and-karma** | Added QUnit tests and [Karma](https://karma-runner.github.io/) based execution
+**solution-add-coverage-and-opa5-tests** | Added OPA5 tests and code coverage based on [Istanbul](https://istanbul.js.org/)
+
